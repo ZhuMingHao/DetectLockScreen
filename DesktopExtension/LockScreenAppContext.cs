6d74fcf4f965425828940d7d9024ec90db6262af
msdnwptest <54531691+msdnwptest@users.noreply.github.com>
2020-04-15 11:15:42 +0800

    Add project files.

---
+++ DesktopExtension/LockScreenAppContext.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+
+
+namespace DesktopExtension
+{
+    class LockScreenAppContext : ApplicationContext
+    {
+
+        private Process process = null;
+        private bool LockScreenInProgress = false;
+        private HotKeyWindow hotkeyWindow = null;
+
+        public LockScreenAppContext()
+        {
+            int processId = (int)ApplicationData.Current.LocalSettings.Values["processId"];
+            process = Process.GetProcessById(processId);
+            process.EnableRaisingEvents = true;
+            process.Exited += HotkeyAppContext_Exited;
+            hotkeyWindow = new HotKeyWindow();
+            SystemEvents.SessionSwitch += new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
+
+        }
+
+        private void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            switch (e.Reason)
+            {
+
+                case SessionSwitchReason.SessionLock:
+                    SentMessage(true);
+                    break;
+                case SessionSwitchReason.SessionUnlock:
+                    SentMessage(false);
+                    break;
+
+            }
+        }
+
+        private async void SentMessage(bool isLocked)
+        {
+            ValueSet ScreebLocked = new ValueSet();
+            ScreebLocked.Add("Lock", isLocked);
+
+            AppServiceConnection connection = new AppServiceConnection();
+            connection.PackageFamilyName = Package.Current.Id.FamilyName;
+            connection.AppServiceName = "LockScreenConnection";
+            AppServiceConnectionStatus status = await connection.OpenAsync();
+            if (status != AppServiceConnectionStatus.Success)
+            {
+                Debug.WriteLine(status);
+                Application.Exit();
+            }
+            connection.ServiceClosed += Connection_ServiceClosed;
+            AppServiceResponse response = await connection.SendMessageAsync(ScreebLocked);
+        }
+
+        private void HotkeyAppContext_Exited(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Connection_ServiceClosed(AppServiceConnection sender, AppServiceClosedEventArgs args)
+        {
+            Debug.WriteLine("Connection_ServiceClosed");
+            LockScreenInProgress = false;
+        }
+
+        private async void hotkeys_HotkeyPressed(int ID)
+        {
+
+            // bring the UWP to the foreground (optional)
+            IEnumerable<AppListEntry> appListEntries = await Package.Current.GetAppListEntriesAsync();
+            await appListEntries.First().LaunchAsync();
+
+            // send the key ID to the UWP
+            ValueSet hotkeyPressed = new ValueSet();
+            hotkeyPressed.Add("ID", ID);
+
+            AppServiceConnection connection = new AppServiceConnection();
+            connection.PackageFamilyName = Package.Current.Id.FamilyName;
+            connection.AppServiceName = "HotkeyConnection";
+            AppServiceConnectionStatus status = await connection.OpenAsync();
+            if (status != AppServiceConnectionStatus.Success)
+            {
+                Debug.WriteLine(status);
+                Application.Exit();
+            }
+            connection.ServiceClosed += Connection_ServiceClosed;
+            AppServiceResponse response = await connection.SendMessageAsync(hotkeyPressed);
+        }
+    }
+    public class HotKeyWindow : NativeWindow
+    {
+      
+       
+
+        // creates a headless Window to register for and handle WM_HOTKEY
+        public HotKeyWindow()
+        {
+            this.CreateHandle(new CreateParams());
+            Application.ApplicationExit += new EventHandler(Application_ApplicationExit);
+        }
+
+      
+
+        private void Application_ApplicationExit(object sender, EventArgs e)
+        {
+            this.DestroyHandle();
+        }
+
+       
+    }
+}
---
+++ DetectLockScreenUWPCore/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DetectLockScreenUWPCore
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0))
+            {
+                Process process = Process.GetCurrentProcess();
+                ApplicationData.Current.LocalSettings.Values["processID"] = process.Id;
+
+                App.AppServiceConnected += AppServiceConnected;
+                await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+
+                MessageDialog dlg = new MessageDialog("Registered");
+                await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                {
+                    await dlg.ShowAsync();
+                });
+            }
+        }
+
+        private void AppServiceConnected(object sender, AppServiceTriggerDetails e)
+        {
+            e.AppServiceConnection.RequestReceived += AppServiceConnection_RequestReceived;
+        }
+
+        private async void AppServiceConnection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            AppServiceDeferral messageDeferral = args.GetDeferral();
+            bool isLock = (bool)args.Request.Message["Lock"];
+            switch (isLock)
+            {
+                case true://stingray
+                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                    {
+                        MessageDialog dlg = new MessageDialog("The Screen Locked");
+                        await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                        {
+                            await dlg.ShowAsync();
+                        });
+
+
+                    });
+                    break;
+                case false://octopus
+                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                    {
+                        MessageDialog dlg = new MessageDialog("The Screen UnLocked");
+                        await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                        {
+                            await dlg.ShowAsync();
+                        });
+
+
+                    });
+                    break;
+                default:
+                    break;
+            }
+            await args.Request.SendResponseAsync(new ValueSet());
+            messageDeferral.Complete();
+
+            // we no longer need the connection
+            App.AppServiceDeferral.Complete();
+            App.Connection = null;
+        }
+
+    }
+}
